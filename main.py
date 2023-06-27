4aba317da20b59d68e5595e83dd4ac1725169311
UFOer <31797946+ufoer168@users.noreply.github.com>
2023-06-27 15:54:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from datetime import datetime
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "robert"}
@@ -10,11 +11,11 @@ def read_root():
 async def read_item(item_id):
     return {"item_id": item_id}
 
-#query parameter
 @app.get("/raspberry")
-async def read_item(light: float = 0.0, temperature: float = 0.0):
+async def read_item(time:str = datetime.now().strftime("%Y%m%d %H:%M:%S"),light: float = 0.0, temperature: float = 0.0):
+    
     return {
-        "時間":datetime.now().strftime("%Y%m%d %H:%M:%S"),
+        "時間":time,
         "光線":light,
         "溫度":temperature
     }
