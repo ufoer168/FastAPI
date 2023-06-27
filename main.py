fdf0adb8a77c24e5b6172afc8715fab33ab5910d
ufoer168 <ufoer168@gmail.com>
2023-06-27 15:53:10 +0800

    20230627

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
-
+from datetime import datetime
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
+    return {"Hello": "robert"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id, q = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+@app.get("/raspberry")
+async def read_item(time:str = datetime.now().strftime("%Y%m%d %H:%M:%S"),light: float = 0.0, temperature: float = 0.0):
+    
+    return {
+        "時間":time,
+        "光線":light,
+        "溫度":temperature
+    }
